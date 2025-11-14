0b3956de3362791c58606c705fe9a43615443b32
Jonathan Maddock <78556175+jonmaddock@users.noreply.github.com>
2025-11-14 14:15:33 +0000

    Fix to obsolete vars

--- process/io/obsolete_vars.py
+++ process/io/obsolete_vars.py
@@ -471,7 +471,7 @@ OBS_VARS = {
     "t_burn": "t_plant_pulse_burn",
     "tfootfi": "f_dr_tf_outboard_inboard",
     "tmargin_tf": "temp_tf_superconductor_margin_min",
-    "tmargin_cs": "temp_cs_superconductor_margin_min",
+    "tmargmin_cs": "temp_cs_superconductor_margin_min",
     "temp_cs_margin": "temp_cs_superconductor_margin",
 }
 
