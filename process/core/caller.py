93234723d10f8898acc771968ad83abe0af078cb
Jonathan Maddock <78556175+jonmaddock@users.noreply.github.com>
2025-11-18 07:53:21 +0000

    Remove closing of idempotence files
    Not using idempotence files now beta FPP in place, causes full mfile output to not be written.

--- process/core/caller.py
+++ process/core/caller.py
@@ -374,6 +374,16 @@ class Caller:
         #             f"\033[93m{non_idempotent_warning}\n{non_idempotent_table}\033[0m",
         #             stacklevel=2,
         #         )
+        # Close idempotence files, write final output file and mfile
+        # OutputFileManager.close_idempotence_files()
+        finalise(
+            self.models,
+            self.data,
+            ifail,
+            # non_idempotent_msg=non_idempotent_warning + "\n" + non_idempotent_table,
+            non_idempotent_msg="no message",
+        )
+        return
 
         #         # Close idempotence files, write final output file and mfile
         #         OutputFileManager.close_idempotence_files(self.data.globals.output_prefix)
