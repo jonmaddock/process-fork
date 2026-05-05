fa8fceebe9a2b20ebcb2a354e05abb7f0fe7bde4
Jonathan Maddock <78556175+jonmaddock@users.noreply.github.com>
2026-05-05 10:01:35 +0100

    Add residual optimiser RMSE output

--- process/data_structure/numerics.py
+++ process/data_structure/numerics.py
@@ -546,6 +546,9 @@ class NumericsData:
     # other python utilities, so they cannot easily be changed.
 
     name_xc: list[str] = field(default_factory=lambda: [""] * IPNVARS)
+    derivative_rmse = 0.0
+    derivatives = np.array([0.0, 0.0])
+    derivatives_norm = np.array([0.0, 0.0])
 
     sqsumsq: float = 0.0
     """sqrt of the sum of the square of the constraint residuals"""
