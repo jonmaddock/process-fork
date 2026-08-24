Perform separate radiation reduction for impurity-radiation-corrected tauE calculation
Leave plasma power balance impurity radiation un-reduced.

process/models/physics/impurity_radiation.py
@@ -661,6 +661,9 @@ class ImpurityRadiation:
         self.pden_impurity_core_rad_profile = np.zeros(
             self.data.physics.n_plasma_profile_elements
         )
+        self.pden_impurity_core_rad_profile_tauE = np.zeros(
+            self.data.physics.n_plasma_profile_elements
+        )
         self.pden_impurity_rad_edge_profile = np.zeros(
             self.data.physics.n_plasma_profile_elements
         )
@@ -668,6 +671,7 @@ class ImpurityRadiation:
         self.pden_impurity_rad_total_mw = 0.0
         self.pden_impurity_core_rad_total_mw = 0.0
         self.pden_impurity_rad_edge_total_mw = 0.0
+        self.pden_impurity_core_rad_total_tauE_mw = 0.0
 
     def run(self):
         """ImpurityRadiation model isn't run"""
@@ -721,10 +725,20 @@ class ImpurityRadiation:
         )
 
         # Core region radiation profile
-        pden_impurity_core_rad_total = (
-            self.pden_impurity_radiation_profile
-            * self.plasma_profile.neprofile.profile_x
-            * fradcore_profile
+        pden_impurity_core_rad_total = self.pden_impurity_radiation_profile * (
+            create_f_rad_core_profile(
+                rho=self.plasma_profile.neprofile.profile_x,
+                radius_plasma_core_norm=self.data.impurity_radiation.radius_plasma_core_norm,
+                f_p_plasma_core_rad_reduction=1.0,
+            )
+        )
+        # Reduce core radiation for tauE calculation only
+        pden_impurity_core_rad_total_tauE = self.pden_impurity_radiation_profile * (
+            create_f_rad_core_profile(
+                rho=self.plasma_profile.neprofile.profile_x,
+                radius_plasma_core_norm=self.data.impurity_radiation.radius_plasma_core_norm,
+                f_p_plasma_core_rad_reduction=self.data.impurity_radiation.f_p_plasma_core_rad_reduction,
+            )
         )
 
         # Edge region radiation profile
@@ -753,6 +767,9 @@ class ImpurityRadiation:
         self.pden_impurity_rad_edge_profile = np.add(
             self.pden_impurity_rad_edge_profile, pden_impurity_rad_edge_total
         )
+        self.pden_impurity_core_rad_profile_tauE = np.add(
+            self.pden_impurity_core_rad_profile_tauE, pden_impurity_core_rad_total_tauE
+        )
 
     def integrate_radiation_loss_profiles(self):
         """Integrate the radiation loss profiles using the Simpson rule.
@@ -770,6 +787,12 @@ class ImpurityRadiation:
         )
         self.pden_impurity_core_rad_total_mw = 2.0e-6 * integrate.simpson(
             self.pden_impurity_core_rad_profile
+            * self.plasma_profile.neprofile.profile_x,
+            x=self.plasma_profile.neprofile.profile_x,
+            dx=self.plasma_profile.neprofile.profile_dx,
+        )
+        self.pden_impurity_core_rad_total_tauE_mw = 2.0e-6 * integrate.simpson(
+            self.pden_impurity_core_rad_profile_tauE
             * self.plasma_profile.neprofile.profile_x,
             x=self.plasma_profile.neprofile.profile_x,
             dx=self.plasma_profile.neprofile.profile_dx,